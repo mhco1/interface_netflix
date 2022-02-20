e9ebf44a88d81710e7074aec541ad7f91f0aa586
MatheusHenrique2000 <matheus.henrique.cortez.oliveira@gmail.com>
2022-02-20 00:03:13 -0300

    adjust logo and middleware.js

--- lo-flix/routes/middleware/middleware.js
+++ lo-flix/routes/middleware/middleware.js
@@ -1,14 +1,1 @@
 module.exports = {}
-
-/* module.exports.processImgMain = function(){
-    var urlImg = "https://i.ytimg.com/vi/5qap5aO4i9A/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB6KAYW8BN9j5s-5nSSxs_g3DdfmA";
-
-    Caman(urlImg, function(req, res, next){
-        this.brightness(5);
-        this.render(function () {
-          this.save("./public/temp/main_background.png");
-        });
-      });
-
-    next();
-} */
