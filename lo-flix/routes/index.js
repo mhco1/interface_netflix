5e56175f9e595c1d3d61471a8f1a1ef9fea1f90c
MatheusHenrique2000 <matheus.henrique.cortez.oliveira@gmail.com>
2022-02-25 16:02:15 -0300

    add router to test

--- lo-flix/routes/index.js
+++ lo-flix/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('home.ejs');
 });
 
+router.get('/test', function(req, res, next) {
+  res.render('test.ejs');
+});
+
 module.exports = router;
